405f8db1f15ba47ef2eb61bce4396e0824dab9f6
Artiom-Evs <evs.a.s@mail.ru>
2023-02-11 22:57:44 +0300

    Add ApplicationUserClaimsPrincipalFactory class
    This class inherited from UserClaimsPrincipalFactory classю This class is for creating claims for custom profile properties.

---
+++ SpaWithAuth/Factories/ApplicationUserClaimsPrincipalFactory.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Options;
+using SpaWithAuth.Models;
+using System.Security.Claims;
+
+namespace SpaWithAuth.Factories;
+
+public class ApplicationUserClaimsPrincipalFactory : UserClaimsPrincipalFactory<ApplicationUser>
+{
+    public ApplicationUserClaimsPrincipalFactory(
+        UserManager<ApplicationUser> userManager, 
+        IOptions<IdentityOptions> optionsAccessor) 
+        : base(userManager, optionsAccessor) { }
+
+    protected override async Task<ClaimsIdentity> GenerateClaimsAsync(ApplicationUser user)
+    {
+        var identity = await base.GenerateClaimsAsync(user);
+        identity.AddClaim(new Claim(nameof(ApplicationUser.FullName), user.FullName));
+        return identity;
+    }
+}
